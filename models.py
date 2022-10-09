d75449e23265c624f0acb7593912912387f76b5c
laszkiewiczp <85294967+laszkiewiczp@users.noreply.github.com>
2022-10-09 20:04:37 +0200

    initial test results + lemmatization added

--- models.py
+++ models.py
@@ -1,4 +1,6 @@
 import numpy as np
+import spacy
+
 from sklearn.metrics.pairwise import pairwise_distances
 from sklearn.feature_extraction.text import TfidfVectorizer
 from typing import Type
@@ -10,16 +12,24 @@ class VectorSpaceModel:
     data stored in a ClinicalTrials object
     '''
     
-    def __init__(self, TrialsData: Type[ClinicalTrials]):
+    def __init__(self, TrialsData: Type[ClinicalTrials],
+                 tokenize: bool = False):
         #Corpus is a list of brief titles
         corpus = list(TrialsData.brief_titles.values())
+        
+        if tokenize:
+            nlp = spacy.load("en_core_web_sm")
+            for idx, document in enumerate(corpus):
+                doc = [token.lemma_ + ' ' for token in nlp(document)]
+                doc_text = ''.join(doc)
+                corpus[idx] = doc_text
 
         #Rearrange id_list so that the order matches brief titles
         id_list = list(TrialsData.brief_titles.keys())
 
         #Learn a vocab of unigrams and bigrams
-        index = TfidfVectorizer(ngram_range=(1, 2), analyzer='word',
-                            stop_words=None)
+        index = TfidfVectorizer(ngram_range=(1, 1), analyzer='word',
+                            stop_words=None, lowercase = False)
         index.fit(corpus)
 
         #Compute the corpus representation
@@ -29,11 +39,17 @@ class VectorSpaceModel:
         self.id_list = np.array(id_list)
         self.index = index
         self.X = X
+        self.tokenize = tokenize
             
     def get_top_query_results(self, query: str) -> list[str]:
         '''
         Function used to find search results for a given query
         '''
+        if self.tokenize:
+            nlp = spacy.load("en_core_web_sm")
+            lemmas = [token.lemma_ + ' ' for token in nlp(query)]
+            query = ''.join(lemmas)
+            
         #Transform query to vector form
         query_tfidf = self.index.transform([query])
         
